ed5b0a235eee4e9800604cd57e23e8ea75d90f2e
Sirferdi <okhaifohferdinard@gmail.com>
2024-01-11 13:55:09 +0100

    Ecommerce

---
+++ src/Pages/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "../../Components/Search/Search";
+
+function Homepage() {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+}
+
+export default Homepage;
